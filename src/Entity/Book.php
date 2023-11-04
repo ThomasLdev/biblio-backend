02548c5231336eacb8ebd691980b1c13f2fa869b
Thomas LEFEBVRE <tlefebvre@norsys.fr>
2023-11-04 12:17:08 +0100

    add api platform core

---
+++ src/Entity/Book.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BookRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Represent a book entity to be fetched by its isbn code. No real data is stored in this entity.
+ */
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $isbn10 = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $isbn13 = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn10(): ?string
+    {
+        return $this->isbn10;
+    }
+
+    public function setIsbn10(?string $isbn10): static
+    {
+        $this->isbn10 = $isbn10;
+
+        return $this;
+    }
+
+    public function getIsbn13(): ?string
+    {
+        return $this->isbn13;
+    }
+
+    public function setIsbn13(?string $isbn13): static
+    {
+        $this->isbn13 = $isbn13;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
